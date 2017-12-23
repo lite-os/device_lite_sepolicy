5dfc3c55a86e16082bd54511d7124dc68423d909
Demon000 <demonsingur@gmail.com>
2017-12-23 15:07:01 +0200

    sepolicy: common: add rules for lineage vibrator hal
    Change-Id: I1e8aba0e0ce1a51cfcd54c734d58f10f0613f773

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -8,3 +8,6 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/lite/sepolicy/common/private
+
+BOARD_SEPOLICY_DIRS += \
+   device/lite/sepolicy/common/vendor
