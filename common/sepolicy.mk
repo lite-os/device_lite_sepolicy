63786b0033499caff9056b81aa6e37e75a3c8a05
aleha-druga <aleha.druga@gmail.com>
2018-05-15 09:41:02 +0000

    sepolicy: Initial repo structure

---
+++ common/sepolicy.mk
@@ -0,0 +1,7 @@
+#
+# This policy configuration will be used by all products that
+# inherit from LiteOS
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/lite/sepolicy/common
---
+++ qcom/sepolicy.mk
@@ -0,0 +1,7 @@
+#
+# This policy configuration will be used by all qcom products
+# that inherit from LiteOS
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/lite/sepolicy/qcom
