LineageOS/android_device_lineage_sepolicy

sepolicy: common: Lol three spaces
Change-Id: Ia420ce23968cd1e245ca8272f5377725658f1f5c

common/sepolicy.mk
@@ -10,4 +10,4 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/lineage/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/common/vendor
+    device/lineage/sepolicy/common/vendor

lite-os/device_lite_sepolicy

common/sepolicy.mk
@@ -10,4 +10,4 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/lite/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-   device/lite/sepolicy/common/vendor
+    device/lite/sepolicy/common/vendor